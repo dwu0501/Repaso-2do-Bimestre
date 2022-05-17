248a466beea10f04a43b5b8a4518eb22b94e65bf
dwu0501 <101563675+dwu0501@users.noreply.github.com>
2022-05-17 16:01:43 -0300

    Initial commit

---
+++ Assets/BrickCloner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BrickCloner : MonoBehaviour
+{
+
+    public GameObject brickPrefab;
+    public float xOffset;
+    int counter = 0;
+    public Text userOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void cloneBrick()
+    {
+        if (counter == 0)
+        {
+            xOffset = float.Parse(userOffset.text);
+        }
+        if(counter<5)
+        {
+        GameObject clon;
+        clon= Instantiate(brickPrefab);
+        clon.transform.position += new Vector3(xOffset, 0, 0);
+        xOffset += 1.1f;
+         counter++;
+        }
+        
+    }
+}
